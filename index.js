59e40cc209a895853dbe7c0c388b2e70268a763f
Luke Storry <luke@lukestorry.co.uk>
2020-04-01 17:02:30 +0100

    added resizing buttons

--- index.js
+++ index.js
@@ -4,19 +4,38 @@ import {
   Component
 } from "https://unpkg.com/htm/preact/standalone.module.js";
 
-const Input = ({ array, updateCallback }) => {
+const ResizerButtons = ({ array, updateCallback }) => {
+  const addWidth = () => updateCallback(array.map(r => r.concat([0])));
 
+  const removeWidth = () => updateCallback(array.map(r => r.slice(0, -1)));
+
+  const addHeight = () =>
+    updateCallback(array.concat([Array(array[0].length).fill(0)]));
+
+  const removeHeight = () => updateCallback(array.slice(0, -1));
+
+  return html`
+    <div>
+      <button onClick=${addWidth}>+ width</button>
+      <button onClick=${removeWidth}>- width</button>
+      <button onClick=${addHeight}>+ height</button>
+      <button onClick=${removeHeight}>- height</button>
+    </div>
+  `;
+};
+
+const Input = ({ array, updateCallback }) => {
   const updateCell = (rowIndex, colIndex) => () => {
     array[rowIndex][colIndex] = array[rowIndex][colIndex] ? 0 : 1;
     updateCallback(array);
   };
-  
+
   const createRow = (row, rowIndex) => html`
     <div>
       ${row.map(
         (active, colIndex) => html`
           <button
-            class=${active ? "active" : ""}
+            class=${active ? "active cell" : "cell"}
             onClick=${updateCell(rowIndex, colIndex)}
           />
         `
@@ -27,6 +46,7 @@ const Input = ({ array, updateCallback }) => {
   return html`
     <section>
       ${array.map(createRow)}
+      <${ResizerButtons} array=${array} updateCallback=${updateCallback} />
     </section>
   `;
 };
@@ -51,11 +71,15 @@ class App extends Component {
   componentWillMount = () =>
     this.setState({
       array: [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
       ]
     });
 
